90d3eabcf0936fa55e01266a7f613c50d818c96c
Gustavo <gustavoguimaraes@gmail.com>
2014-01-07 18:56:32 -0500

    Add demo_app

--- demo_app/config/initializers/secret_token.rb
+++ demo_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'd02752efb0a1e732f069ec649215547bb8d038a4c9da4b7e1b8363998d92455b6c73dd68d599399a4a8bb7b3feab20531abdeea9c0f6667ec5df12ed5999e312'
+DemoApp::Application.config.secret_token = '2e220bf60d77885e2071e7032c97f38b2b8eb8ae21acfbae3317de47fbf34aa547b2c6191c9ebf286db669f5c6b2e74ea2177cfe11262054e457fca711ff4639'
--- demo_app/db/schema.rb
+++ demo_app/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140106200343) do
+ActiveRecord::Schema.define(:version => 20140107233110) do
 
-  create_table "posts", :force => true do |t|
-    t.text     "body"
-    t.string   "title"
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
